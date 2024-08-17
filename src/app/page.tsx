0dc2392c5d66c8d51bf0e4111b9cc6c351889603
OmDwivedi1237 <26dwivediom@gmail.com>
2024-08-16 22:38:15 -0700

    Fixed padding as huge hydration issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <HeroHighlight className="flex flex-col items-center justify-center">
+    <HeroHighlight className="flex flex-col items-center justify-center min-h-screen p-4">
       <motion.h1
         initial={{
           opacity: 0,
@@ -21,13 +21,15 @@ export default function Home() {
         }}
         className="text-8xl px-2 md:text-8xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto"
       >
-        With FlashMind AI, no knowledge is lost. Every card
-        is a{"\n"}
-        <Highlight className="text-black dark:text-white">
-          gateway to effortless recall.
-        </Highlight>
+        With FlashMind AI, no knowledge is lost. Every card is a
+        <br />
+        <span className="block mt-4">
+          <Highlight className="text-black dark:text-white">
+            gateway to effortless recall.
+          </Highlight>
+        </span>
       </motion.h1>
-      <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-3 transition duration-400 mt-8">
+      <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400 mt-10">
         Get Started
       </button>
     </HeroHighlight>
